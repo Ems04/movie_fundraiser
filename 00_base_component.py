ce54a40cc64a21c8bb2f37891462c13269fe8f0d
Ems04 <smith.eliza@awatapu.school.nz>
2021-09-15 13:25:30 +1200

    Base component is up to date (ticket_price).

--- 00_base_component.py
+++ 00_base_component.py
@@ -15,22 +15,20 @@ def not_blank(question):
             print("This can't be blank, please enter your name.")
 
 # integer check
-def int_check(question,low,high):
+def int_check(question):
 
-    error = "Please enter a whole number between {} and {}".format(low,high)
+    error = "Please enter a whole number between 12 and 130"
 
     valid = False
     while not valid:
-
     # ask user for number and check it's valid
         try:
             response = int(input(question))
 
-            if low <= response <= high:
-                return response
-            else:
+            if response <=0:
                 print(error)
-
+            else:
+                return response
     # if an integer is not entered, display an error message
         except ValueError:
             print(error)
@@ -38,30 +36,63 @@ def int_check(question,low,high):
 # Get up dictionaries / lists needed to hold data
 # Ask user if they have used the program before
 # Loop to get ticket detail
-    # initialise loop so that it runs at least oncea
-name = ""
-count = 0
+    # initialise loop so that it runs at least once
 MAX_TICKETS = 5
+profit = 0
+ticket_profit = 0
+name = ""
+ticket_count = 0
+ticket_sales = 0
 
-while name != 'xxx' and count < MAX_TICKETS:
-    print("You have {} seats left.".format(MAX_TICKETS - count))
+while name != 'xxx' and ticket_count < MAX_TICKETS:
+    if ticket_count < MAX_TICKETS:
+        print("You have {} seats left.".format(MAX_TICKETS - ticket_count))
+    else:
+        print("You have one place left")
 
     # Get details...
-
     # Name (can't be blank)
     name = not_blank("Name: ")
-    count += 1
+    if name == "xxx":
+        break
     # Age (between 12 & 130)
-    age = int_check("Age: ", 12, 130)
+    age = int_check("Age: ")
 
-    if count == MAX_TICKETS:
-        print("You have sold all the available tickets!")
+    # check that age is valid
+    if age < 12:
+        print("Sorry you are too young to watch this movie.")
+        continue
+    elif age > 130:
+        print("This is very old. It looks like you have made a mistake.")
+        continue
+
+    # ticket price details
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
     else:
-        print("You have sold {} tickets. There are {} place still available.".format(count, MAX_TICKETS - count))
+        ticket_price = 6.5
 
+    ticket_count += 1
+    ticket_sales += ticket_price
 
-        # Calculate ticket price
-        # Ask for paymethod (ands apply surcharge if need)
-    # Calculate total sales and profit
-    # Output data to text file
+# End of ticket loop
+# Calculalty profit
+ticket_price = ticket_count - (5 * ticket_count)
+print("Profit from tickets sales are ${:.2f}".format(ticket_sales))
+
+# Tell user if they have unsold tickets
+if ticket_count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets. There are {} place still available.".format(ticket_count, MAX_TICKETS - ticket_count))
+
+# Calculate ticket price
+profit_made = ticket_price - 5
+profit += profit_made
+
+# Ask for paymethod (ands apply surcharge if need)
+# Calculate total sales and profit
+# Output data to text file
 
