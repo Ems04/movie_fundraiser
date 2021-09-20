9fcd53950705a23dd96734d6e1d5169925e79c2b
Ems04 <smith.eliza@awatapu.school.nz>
2021-09-20 14:22:21 +1200

    Basic string check is working

---
+++ 05_yes_no_string.py
@@ -0,0 +1,22 @@
+def yes_no (question):
+
+    error = "Please answer with yes or no."
+
+    valid = False
+    while not valid:
+
+        # Ask question and put response is lowercase
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print(error)
+
+# Main routine goes here
+for item in range(0, 6):
+    want_snack = yes_no("Would like to have some snacks?")
+    print("Answer okay. You said: {}".format(want_snack))
+    print()
